ed05c050544f2393ab53c0795b90ee9ed84861c6
zhensanwuhou <982866235@qq.com>
2019-09-23 19:37:02 +0800

    请问情

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,16 +1,23 @@
 package com.example.demo;
 
+import com.example.demo.controller.HelloWorldController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.Resource;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
 
+	@Resource
+	private HelloWorldController helloWorldController;
+
 	@Test
-	public void contextLoads() {
+	public void getHello() throws Exception {
+		System.out.println(helloWorldController.index());
 	}
 
 }
